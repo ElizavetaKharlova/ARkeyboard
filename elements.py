ef0209dec9755bf565f87f8b9bc87e4534f38e86
Peter Atrazhev <atrazhev@ualberta.ca>
2018-10-12 09:52:09 -0600

    Added a elements

---
+++ elements.py
@@ -0,0 +1,50 @@
+# this
+
+class keyboard():
+
+    '''
+This class generates a keyboard on the screen at the x and y Pixel locations
+'''
+    def display(self,x,y):
+        '''
+
+        :param x:
+        :param y:
+        :return:
+        '''
+
+class timer():
+    '''
+
+'''
+    def display(self, x, y):
+        '''
+
+        :param x:
+        :param y:
+        :return:
+        '''
+class targetDisplay():
+    '''
+
+    '''
+
+    def display(self, x, y):
+        '''
+
+        :param x:
+        :param y:
+        :return:
+        '''
+class leaderboard():
+    '''
+
+
+    '''
+    def display(self, x, y):
+        '''
+
+        :param x:
+        :param y:
+        :return:
+        '''
