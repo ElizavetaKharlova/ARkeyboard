5ebd04d69ede72cb5769990215d4dd99283692b0
DCMay <dcmay@ualberta.ca>
2018-10-17 11:46:49 -0600

    Daniel Master
    the master file will call the rest and govern the Rest, Onboarding and Game loops

---
+++ MasterFile.py
@@ -0,0 +1,19 @@
+#This master file will feature 3 modes:
+#-----------------------------------------------------------------------------------------------------------------------
+# Mode Rest:
+#  calls the 'getLeaderboard' function, displays a leaderboard,
+#  Displays a prompt to onboard by pressing space,
+#  Displays the option to terminate by pressing escape
+#-----------------------------------------------------------------------------------------------------------------------
+# Mode Onboard:
+#  displays and calls the keyboard
+#  prompts to type ur name by using the keyboard
+#  promts to use space to start Game
+#  prompts to use escape to return to Rest
+#-----------------------------------------------------------------------------------------------------------------------
+# Mode Game:
+# calls the "getTarget"function, displays the target
+# calls the 'getKeyTyped' function and displays the sequence typed
+# calls the 'getAccuracy' function and displays the accuracy
+# calls the 'getTimer' function and displays the Time
+# prompts to quit by using escape
