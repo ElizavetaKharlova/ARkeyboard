af67ab2e0a46c4779fcd5165abbed63d661dd8d7
Steven <shida1@ualberta.ca>
2018-10-17 16:53:45 -0600

    kb tester
    included function to draw kb - for peter's display

--- kb_class_test.py
+++ kb_class_test.py
@@ -15,6 +15,10 @@ cap = cv2.VideoCapture(0)
 ret, video_frame = cap.read()
 kb.init_kb_params(kb_layout, video_frame)
 
+def draw_key(kb, letter, frame=None):
+    k_params = kb.get_key_params(letter)
+    cv2.rectangle(frame, k_params['k_start_coords'], k_params['k_end_coords'], (255,255,255), 3)
+    cv2.putText(frame, letter, k_params['l_coords'], font, 0.5, (200,255,255), 1, cv2.LINE_AA)
 
 
 while True:
@@ -26,6 +30,9 @@ while True:
     font = cv2.FONT_HERSHEY_COMPLEX
 
     # Draw keyboard
+    for row in range(kb.graphic['kb_shape'][0]):
+        for col in range(kb.graphic['kb_shape'][1]):
+            draw_key(kb, kb.layout[row][col], img)
     # for row in range(kb_params[4][0]):
     #     for col in range(kb_params[4][1]):
     #         put_button(kb_layout, kb_layout[row][col], kb_params, frames)
@@ -44,7 +51,8 @@ while True:
 #     cv2.putText(img, type_word, (900,300), font, 1, (200,255,255), 2, cv2.LINE_AA)
 #
     active_key = kb.detect_key(det)
-    print(active_key)
+    if active_key is not None:
+        print(active_key)
     cv2.imshow('img',img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
